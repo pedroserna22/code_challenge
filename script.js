90ab6982ae665137ec8283372d4b96533774a2df
pedroserna22 <pserna364@gmail.com>
2023-09-05 18:56:27 -0300

    solucion_codchallv2

--- script.js
+++ script.js
@@ -4,24 +4,39 @@
 /Se utiliza fetch para enviar una solicitud POST al servidor con la URL especificada y los datos del formulario en formato JSON.
 /Y por ultimo se maneja la respuesta del servidor en las funciones .then() y se muestra en la consola.
 */
-const form = document.getElementById('form-regis');
-const nombreInput = document.getElementById('nombre');
-const apellidoInput = document.getElementById('apellido');
-const fechaNacimientoInput = document.getElementById('fechaNacimiento');
-
-const data = {
-    nombre: nombreInput.value,
-    apellido: apellidoInput.value,
-    fechaNacimiento: fechaNacimientoInput.value
-};
-
-fetch('https://jsonplaceholder.typicode.com/users', {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(data)
-})
-.then(response => response.json())
-.then(data => console.log(data))
-.catch(error => console.error('Error:', error));
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.getElementById('form-regis');
+  
+    form.addEventListener('submit', function (event) {
+        event.preventDefault(); // Evita la recarga de la página
+  
+        // Se obtiene la referencia de lo que ingresa el usuario en los input
+        const nombre = document.getElementById('nombre').value;
+        const apellido = document.getElementById('apellido').value;
+        const fechaNacimiento = document.getElementById('fechaNacimiento').value;
+  
+        // Objeto JSON con los valores de los input
+        const data = {
+            nombre: nombre,
+            apellido: apellido,
+            fechaNacimiento: fechaNacimiento
+        };
+  
+        // Realiza la solicitud HTTP POST con la siguiente URL
+        fetch('https://jsonplaceholder.typicode.com/users', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        .then(response => response.json())
+        .then(data => {
+            // Con un console manejamos la respuesta del servidor para mostrar la persona ingresada
+            console.log(data);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+    });
+  });
